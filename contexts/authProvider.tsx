9599f70d2396b58821968848faf0829827335a3d
Akinsanmi <akintunde60@gmail.com>
2022-12-18 18:03:19 +0100

    authcontext adjustment

--- contexts/authProvider.tsx
+++ contexts/authProvider.tsx
@@ -1,30 +1,26 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, {createContext, useState} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type UserProp = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address: string;
+  state: string;
+  lga: string;
+  gender: string;
+  _id: string;
+  userIdNos: string;
+};
 
 type AuthUserType = {
-  user: {
-    email: string;
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-    address: string;
-    state: string;
-    lga: string;
-    gender: string;
-    _id: string;
-    userIdNos: string;
-    accountType: string;
-  };
+  user: UserProp;
   token?: string;
-  transaction?: {
-    reference: string;
-    status: string;
-    trans: string;
-    message: string;
-  };
 };
 
 type ContextType = {
@@ -38,20 +34,25 @@ type ProviderProps = {
 };
 const AuthContext = createContext({} as ContextType);
 
-export const pushToLocalStorage = (token: string, user?: any) => {
-  localStorage.setItem("token", token);
-  localStorage.setItem("user", JSON.stringify(user));
+export const pushToLocalStorage = async (token: string, user: UserProp) => {
+  try {
+    await AsyncStorage.setItem("token", token);
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const getUser = () => {
-  // const userToken = localStorage.getItem("token");
-  const userString = localStorage.getItem("user") as string;
+const getUser = async () => {
+  const userToken = await AsyncStorage.getItem("@storage_Key");
+  const userGotten = await AsyncStorage.getItem("@storage_User");
+  const userString = JSON.parse(userGotten!);
 
-  return {userString};
+  return {userString, userToken};
 };
 
-const {userString} = getUser();
-const user = JSON.parse(userString);
+const {userString} = await getUser();
+const user = userString as UserProp;
 
 export function AuthProvider({children}: ProviderProps) {
   const [authUser, setAuthUser] = useState<AuthUserType | null>({
@@ -61,7 +62,7 @@ export function AuthProvider({children}: ProviderProps) {
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   const logout = () => {
-    localStorage.clear();
+    AsyncStorage.clear();
     setAuthUser(null);
     navigation.navigate("Login");
   };
